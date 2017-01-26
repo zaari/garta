fe5141ebff726bf116151150412d84b6e07db7f4
Timo Saarinen <zaari@users.noreply.github.com>
2017-01-26 19:29:32 +0200

    Smooth inertial scrolling implemented to map canvas. PixelPos(i64, i64) replaced with Vector(f64, f64). Tile request order algorithm enhanced.

---
+++ src/core/misc.rs
@@ -0,0 +1,30 @@
+// Garta - GPX viewer and editor
+// Copyright (C) 2016-2017, Timo Saarinen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+extern crate serde;
+extern crate serde_json;
+extern crate chrono;
+
+use std::time::{Duration};
+
+/// Convert duration to milliseconds
+pub fn duration_to_seconds(i: &Duration) -> f64 {
+    let secs = i.as_secs() as f64;
+    let nsecs = i.subsec_nanos() as f64;
+    (secs + 0.000000001 * nsecs)
+}
+
+
